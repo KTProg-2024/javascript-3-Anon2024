34c77d0c569a7cccc4f10fadcd4df70dfa0bdd0d
Anon Unseethaharuthai <>
2021-05-28 13:01:04 +0000

    I finished the assignment

--- motion.js
+++ motion.js
@@ -2,13 +2,27 @@ function myMove() {
   var elem = document.getElementById("animate");   
   var posTop = 0;
   var posLeft = 0;
-  var id = setInterval(frame, 5);
+  var id = setInterval(frame, 1);
   function frame() {
 	  posTop++;
 	  posLeft++;
 	  elem.style.top = posTop + "px"; 
 	  elem.style.left = posLeft + "px"; 
 	  if ((posTop == 350) && (posLeft == 350))
+		  clearInterval(id);
+  }
+}
+function myMove2() {
+  var elem = document.getElementById("blue");   
+  var posTop = 0;
+  var posLeft = 350;
+  var id = setInterval(frame, 1);
+  function frame() {
+	  posTop++;
+	  posLeft--;
+	  elem.style.top = posTop + "px"; 
+	  elem.style.left = posLeft + "px"; 
+	  if ((posTop == 350) && (posLeft == 0))
 		  clearInterval(id);
   }
 }
